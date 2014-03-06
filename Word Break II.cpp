149e458912bc14d12c799f85b19fd2fda15f4e98
Yi Lu <luyi0619@outlook.com>
2014-03-06 13:15:55 +0800

    word break II

---
+++ Word Break II.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<string> answer;
+    vector<string> dic;
+    vector<int> dp;
+    string gs;
+    int n, m;
+    
+    void dfs(int cur, string print){
+        if(dp[cur] == 0) return;
+        if(cur == 0)
+        {
+            answer.push_back(print);
+            return;
+        }
+        int m = dic.size();
+        for(int j = 0 ; j < m ; j ++)
+        {
+            int k = cur - dic[j].size();
+            if( k >= 0 &&  dp[k] && gs.substr(k, dic[j].size()) == dic[j])
+            {
+                dfs(k, dic[j] + (print.size() == 0 ? "" : " ") + print);
+            }
+        }
+    }
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        gs = s;
+        for(unordered_set<string>::iterator it = dict.begin() ; it != dict.end(); it ++){
+            dic.push_back(*it);
+        }
+        n = s.size();
+        m = dic.size();
+        
+        dp = vector<int>(n + 1, 0);
+        dp[0] = 1;
+        for(int i = 0 ;i < n ; i ++)
+        {
+            if(dp[i] == 0) continue;
+            for(int j = 0 ; j < m ; j ++)
+            {
+                int k = i + dic[j].size();
+                if(k <= n && s.substr(i, dic[j].size()) == dic[j])
+                    dp[k] = 1;
+            }
+        }
+        dfs(n,"");
+        return answer;
+    }
+};
