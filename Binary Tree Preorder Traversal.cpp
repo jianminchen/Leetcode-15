b4b4dd5efadf978f2e6cf1112ac8ba40bd5848f7
Yi Lu <luyi0619@users.noreply.github.com>
2015-07-14 17:06:11 +0800

    Update Binary Tree Preorder Traversal.cpp

--- Binary Tree Preorder Traversal.cpp
+++ Binary Tree Preorder Traversal.cpp
@@ -11,19 +11,37 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ret;
-        
+        if (root == NULL) return ret;
         stack<TreeNode*> stk;
         stk.push(root);
-        while(!stk.empty())
-        {
-            TreeNode* cur = stk.top();
+        TreeNode* cur, *prev = root;
+        while (!stk.empty()) {
+            cur = stk.top();
             stk.pop();
-            if(cur != NULL)
-            {
-                ret.push_back(cur->val);
-                stk.push(cur->right);
-                stk.push(cur->left);
+
+            if (prev == cur->left){ // from left
+                if(cur->right != NULL){
+                    stk.push(cur);
+                    stk.push(cur->right);
+                }
             }
+            else if(prev == cur->right){ // from right
+                ;// do nothing
+            }
+            else{ // from root
+                ret.push_back(cur->val);
+                if(cur->left != NULL){
+                    stk.push(cur);
+                    stk.push(cur->left);
+                }
+                else{
+                    if(cur->right != NULL){
+                        stk.push(cur);
+                        stk.push(cur->right);
+                    }
+                }
+            }
+            prev = cur;
         }
         return ret;
     }
